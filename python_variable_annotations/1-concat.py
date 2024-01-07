fc279b2315aaccc241a13d9cd75ba0b4b79dfd85
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 10:10:08 +1100

    fix: added comments

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function 'concat' which
+concatenates two strings together """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function - Concat """
+    return str1 + str2
