c20c1d461d1d7c994f6ccc3dbd9f581bf1b14542
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-07 20:42:32 +1100

    added directory and readme

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""Type-annotated function 'add' which adds floats a and b"""
+def add(a: float, b: float) -> float:
+    return a + b
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+We are learning about Python Variable Annotations
+
+It is important for documentation, debugging and other stuff
