ea9e22fe4e2db200df5fd855360a670db8548bd4
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 17:41:30 +1100

    fix: shortened comment and import

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-""" takes a list mxd_lst of integers and floats
-returns sum as a float."""
-from typing import List
+""" Mixed list function """
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ sum_mixed_list function """
     return sum(mxd_lst)
