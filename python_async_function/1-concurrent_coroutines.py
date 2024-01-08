40b48ec154b05a37dc6ba1dc58b643689dcfefe8
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 19:48:47 +1100

    fix: iteration

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Function that returns the list of delays """
-    tasks = [asyncio.create_task(wait_random(max_delay) for i in range(n))]
+    tasks = [asyncio.create_task(wait_random(max_delay) for _ in range(n))]
     # Creating a list of task objects, each will run wait_random concurrently
     completed_tasks = await asyncio.gather(*tasks)
     # Awaiting the completion of all the tasks and gathering their results
