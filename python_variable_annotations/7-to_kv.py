f77846c351f1d15624485737dd1c543e3b81c0cd
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 17:57:28 +1100

    fix: pycodestyle styling

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@ returns tuple """
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Complex types - string and int/float to tuple """
     return (k, v * v)
