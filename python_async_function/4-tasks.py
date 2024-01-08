a26d8a6799e921a961c5ddbead24aff9dd70496e
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 10:02:43 +1100

    feat: added task 3 and 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Takes code wait_n, alter to new func task_wait_n """
+import asyncio
+import random
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ takes 2 args, n & max_delay then returns the list of delays """
+    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    # Creating a list of task objects, each will run wait_random concurrently
+    completed_tasks = await asyncio.gather(*tasks)
+    # Awaiting the completion of all the tasks and gathering their results
+    return sorted(completed_tasks)
+    # Returning the list of completed tasks' results (delays)
