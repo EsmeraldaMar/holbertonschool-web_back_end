18f472ab029d7177b5a5057c305682e6dc0ba979
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 17:56:06 +1100

    feat: added complete type task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" to_kv function, takes int, str or float
+returns tuple """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+    """ Complex types - string and int/float to tuple """
+    return (k, v * v)
