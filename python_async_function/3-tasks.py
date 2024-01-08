9bfef9ee492bcab4da4dba91f31f310c9f21f556
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 10:04:56 +1100

    fix: pycodestyle styling

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """int max_delay as arg, returns asyncio.Task"""
     return asyncio.create_task(wait_random(max_delay))
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,9 +6,11 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ takes 2 args, n & max_delay then returns the list of delays """
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    tasks = [asyncio.create_task(task_wait_random(max_delay))
+             for _ in range(n)]
     # Creating a list of task objects, each will run wait_random concurrently
     completed_tasks = await asyncio.gather(*tasks)
     # Awaiting the completion of all the tasks and gathering their results
