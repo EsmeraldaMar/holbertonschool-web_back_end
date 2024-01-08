f0e19c9522aeaa3d97bf352050b20d70188c03a0
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-08 17:32:47 +1100

    feat: added sum_list func

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Func sum_list, takes list of floats returns sum """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Function sum_list"""
+    return sum(input_list)
