532028ea3848bf258afb3036ea4b0e57014288ae
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 15:32:13 +1100

    fix: pycode style

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """ collects 10 random numbers using async
-returns 10 random numbers"""
+returns 10 random numbers """
 import asyncio
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """ Collects 10 random numbers then returns it """
-    # Returns 10 numbers collected, we ar iterating
+    # Returns 10 numbers collected, we are iterating
     return [i async for i in async_generator()]
