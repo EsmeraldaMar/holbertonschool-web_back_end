1c1336a0af50bf12a46900c07157304d0fd09c27
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 15:02:56 +1100

    feat: added readme and task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Created async generator, no args, loops 10 times and waits 1 second"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Loops 10 times, yields random num """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
