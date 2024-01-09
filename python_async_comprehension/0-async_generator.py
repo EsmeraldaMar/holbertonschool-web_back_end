54f59153b3c72b0e9b1bdabcc7abf769965911f4
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 15:04:58 +1100

    fix: type annot

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """ Loops 10 times, yields random num """
     for _ in range(10):
         await asyncio.sleep(1)
