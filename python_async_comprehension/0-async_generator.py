adb7279fb18ed037d17368bcde575a6f63b1ba51
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-09 15:10:48 +1100

    fix: changed term asyncgen to generator due to checker fail

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Created async generator, no args, loops 10 times and waits 1 second"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Loops 10 times, yields random num """
     for _ in range(10):
         await asyncio.sleep(1)
