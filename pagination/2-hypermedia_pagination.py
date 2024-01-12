6c4587e5793e0e4239993746fcdc14e672da8ce4
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-12 18:10:44 +1100

    fix: pyc

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -6,10 +6,10 @@ from typing import List, Dict, Any
 
 
 def index_range(page: int, page_size: int) -> tuple:
-        """return tuple of size two containing start index and an end index"""
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        return (start_index, end_index)
+    """return tuple of size two containing start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
