132e2a61de311722a076c23c2635df1ffc3ebd8e
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-17 21:07:36 +1100

    fix: sequencing

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-underscore-dangle */
-/* eslint-disable no-return-assign */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    // eslint-disable-next-line no-underscore-dangle
     this._floors = floors;
   }
 
@@ -14,6 +12,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
