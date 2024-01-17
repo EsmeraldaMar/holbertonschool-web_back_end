b5c04dfe8d06760e7a852ccb586ada83722364fd
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-17 21:21:40 +1100

    added task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-return-assign */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    return this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    return this._code = newCode;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+}
