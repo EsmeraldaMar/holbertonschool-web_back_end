3b2a607415fb0f050a46077f571485f4a3f14ebb
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-25 00:09:58 +1100

    added task 2, used then finally and catch

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* eslint-disable indent */
+export default function handleResponseFromAPI(promise) {
+  return promise
+  .then(() => ({ body: 'success', status: 200 }))
+  .catch(() => new Error())
+  .finally(console.log('Got a response from the API'));
+}
