d565d01920aae9365cdd114f9a935995c770c505
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-30 10:39:05 +1100

    trying uwes code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,16 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+/* eslint-disable */
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: `${error.message}` }));
-
-  const photoPromise = uploadPhoto(fileName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: `${error.message}` }));
-
-  return Promise.all([userPromise, photoPromise]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === "rejected") {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+        }
+      }
+      return values;
+    });
 }
