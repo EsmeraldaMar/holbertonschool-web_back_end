16301a3d428d616511bac7767f77970a6dcf207b
EsmeraldaMar <6580@holbertonstudents.com>
2024-01-31 22:09:24 +1100

    added task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function itemsList() {
+  const items = new Map();
+  items.set('Apples', 10);
+  items.set('Tomatoes', 10);
+  items.set('Pasta', 1);
+  items.set('Rice', 1);
+  items.set('Banana', 5);
+  return items;
+}
